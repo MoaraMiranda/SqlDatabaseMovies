3967d6099886e8bbf61e70db5685ed57912ebc64
moara <moaramirandass@gmail.com>
2023-08-14 12:57:02 +0200

    Adding password bcrypt and treating user error

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,27 @@
-const AppError = require("../utils/AppError")
+const { hash } = require("bcryptjs");
+const knex = require("../database/knex");
+
+const AppError = require("../utils/AppError");
+const connection = require("../database/knex");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if(!name){
-      throw new AppError("Name is required")
+    const hashedPassword = await hash(password, 8);
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const checkUserExists = await knex.select("email").from("users")
+    
+    if (checkUserExists) {
+      throw new AppError("Email already registered");
     }
 
-    response.status(201).json({ name, email, password });
+    response.status(201).json();
   }
 }
 
