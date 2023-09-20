3c6fcb159212f0d746d0b72d29a10caa55b53220
moara <moaramirandass@gmail.com>
2023-09-20 13:09:14 +0200

    JWT install

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRoute = require("./users.route");
 const movieNotesRoute = require("./movieNotes.route");
 const tagsRoute = require("./tags.route");
+const sessionsRoute = require("./sessions.route");
 
 const routes = Router();
 
 routes.use("/users", usersRoute);
+routes.use("/sessions", sessionsRoute);
 routes.use("/movieNotes", movieNotesRoute);
 routes.use("/tags", tagsRoute);
 
