06f1c46fdf289d045d2fdb125bbe8ff8f6201000
moara <moaramirandass@gmail.com>
2023-09-20 13:26:26 +0200

    JWT installed and configured

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const { compare } = require("bcryptjs")
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
 
 class SessionsController {
   async create(request, response) {
@@ -12,13 +14,19 @@ class SessionsController {
       throw new AppError("Invalid email or password.", 401);
     }
 
-    const passwordMatched = await compare(password, user.password)
+    const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
       throw new AppError("Invalid email or password.", 401);
     }
-    
-    return response.json(user);
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
 module.exports = SessionsController;
