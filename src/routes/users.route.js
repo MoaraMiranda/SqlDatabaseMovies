a7f92ec65fa09e36b7f14cac0b5f00d69eda56c6
moara <moaramirandass@gmail.com>
2023-10-05 14:49:55 +0200

    updated project

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,12 +1,23 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoute = Router();
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig.MULTER);
 
 usersRoute.post("/", usersController.create);
 usersRoute.put("/", ensureAuthenticated, usersController.update);
 usersRoute.delete("/", ensureAuthenticated, usersController.delete);
+usersRoute.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+);
 
 module.exports = usersRoute;
