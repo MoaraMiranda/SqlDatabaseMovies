bee7acdbad3260b12538a58e8a68c328dc723ae3
moara <moaramirandass@gmail.com>
2023-11-14 12:25:45 +0100

    Adding beforeEach

--- src/services/userCreateService.spec.js
+++ src/services/userCreateService.spec.js
@@ -1,16 +1,44 @@
-const UserCreateService = require("./UserCreateService")
+const UserCreateService = require("./UserCreateService");
 const UserRepositoryInMemory = require("../repositories/UserRepositoryInMemory");
+const AppError = require("../utils/AppError");
 
-it("user should  be created", async () => {
-const user = {
-  name: "User Test",
-  email: "user@test.com",
-  password: "123"
-}
+describe("UserCreateService", () => {
+  let userRepository = null;
+  let userCreateService = null;
 
-const userRepositoryInMemory = new UserRepositoryInMemory();
-const userCreateService = new UserCreateService(userRepositoryInMemory);
-const userCreated = await userCreateService.execute(user)
+  beforeEach(() => {
+    userRepository = new UserRepositoryInMemory();
+    userCreateService = new UserCreateService(userRepository);
+  });
 
-expect(userCreated).toHaveProperty("id")
-})
+  it("user should  be created", async () => {
+    const user = {
+      name: "User Test",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const userCreated = await userCreateService.execute(user);
+
+    expect(userCreated).toHaveProperty("id");
+  });
+
+  it("user cannot be created using an email already in use", async () => {
+    const user1 = {
+      name: "User Test 1",
+      email: "user@test.com",
+      password: "123",
+    };
+
+    const user2 = {
+      name: "User Test 2",
+      email: "user@test.com",
+      password: "456",
+    };
+
+    await userCreateService.execute(user1);
+    await expect(userCreateService.execute(user2)).rejects.toEqual(
+      new AppError("Email already registered")
+    );
+  });
+});
